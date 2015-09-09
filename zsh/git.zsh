ec7fdb1ed247a75ff69ca2df244747dae8f8eb05
Derek Ahn <derek@sudokrew.com>
2015-09-09 10:24:56 -1000

    updating alias

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -5,5 +5,5 @@ alias ugp="git reset HEAD@{1}"
 # Git-Crypt
 alias gcrypt-init="git-crypt init"
 alias gcrypt-allow="git-crypt add-gpg-user"
-alias gcrypt-attr="touch .gitattributes && echo 'secretfile filter=git-crypt diff=gitcrypt\n.env filter=git-crypt diff=git-crypt' > .gitattributes"
+alias gcrypt-attr="touch .gitattributes && echo '.env filter=git-crypt diff=git-crypt' > .gitattributes"
 alias gdecrypt="git-crypt unlock"

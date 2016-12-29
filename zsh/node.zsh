fcd8af311856c22435c26d5b6bc1f3d925cf5674
Derek Ahn <git.derek@gmail.com>
2016-12-28 21:47:39 -0800

    Removing node.zsh aliases (Deprecated)

--- zsh/node.zsh
+++
@@ -1,4 +0,0 @@
-# Node Aliases
-alias sequelize-migrate="./node_modules/.bin/sequelize --migrate"
-alias sequelize-undo="./node_modules/.bin/sequelize --migrate --undo"
-alias npmcc="npm cache clean"

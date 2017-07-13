97ec5dfbb443f244bd410c785f62234defd1bba4
Derek Ahn <git.derek@gmail.com>
2017-07-12 21:06:47 -0700

    Add neovim, python3, tig to the list

--- exec/install.sh
+++ exec/install.sh
@@ -27,32 +27,41 @@ echo "Installing homebrew package manager"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 echo "Updateing brew"
 brew update
-echo "Checkup brew"
+brew doctor
 
 brew install ack
 brew install autoconf
 brew install automake
 brew install ccrypt
 brew install cscope
-brew install htop
+brew install exercism
 brew install go
+brew install htop
 brew install macvim
+brew install n
 brew install p7zip
 brew install postgresql
-brew install python
-brew install readline
+brew install python3
 brew install ranger
+brew install readline
 brew install reattach-to-user-namespace
 brew install speedtest_cli
 brew install the_silver_searcher
-brew install tmux
+brew install tig
 brew install tldr
+brew install tmux
 brew install vim
 brew install xz
 brew install z
-brew install n
 brew install zsh-syntax-highlighting
-brew install exercism
+
+brew tap neovim/homebrew-neovim
+pip3 install neovim --upgrade
+
+echo "Checkup brew"
+brew cleanup
+brew prune
+brew doctor
 
 echo "Installing Nerd-fonts to ~/Library/Fonts"
 cd ~/Library/Fonts                                                                                      && \

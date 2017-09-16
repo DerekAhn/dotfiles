6d3f0a3df21c11a8f7e5cc3998ba81facbc39300
Derek Ahn <git.derek@gmail.com>
2017-09-15 22:41:14 -0700

    Update exec/link script

--- exec/link.sh
+++ exec/link.sh
@@ -3,9 +3,24 @@
 DOTFILES=$HOME/.dotfiles
 
 echo "Creating symlinks"
-
 for file in **/*.symlink ; do
   target="$HOME/.$( basename $file ".symlink" )"
   echo "creating symlink for $file"
   ln -s $DOTFILES/$file $target
+done
+
+echo -e "\n\nInstalling to ~/.config"
+echo "=============================="
+if [ ! -d $HOME/.config ]; then
+  echo "Creating ~/.config"
+  mkdir -p $HOME/.config
+fi
+for config in $DOTFILES/config/* ; do
+  target=$HOME/.config/$( basename $config )
+  if [ -e $target ]; then
+    echo "~${target#$HOME} already exists... Skipping."
+  else
+    echo "Creating symlink for $config"
+    ln -s $config $target
+  fi
 done

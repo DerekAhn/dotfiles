e7ec91394ce191dcd20cdedf0fa67f3f6ac9629a
Derek Ahn <git.derek@gmail.com>
2019-07-16 19:08:50 -0700

    Add an alias for go tooling gol = go list

--- zsh/go.zsh
+++ zsh/go.zsh
@@ -3,5 +3,6 @@
 
 alias gt="go test ."
 alias gob="go test --bench ."
+alias gol="go list -f '{{ .Dir }}' -m github.com/golang/protobuf"
 alias gtc="go test ./... -coverprofile cp.out"
 alias gtcm="grep -v -e 1$ cp.out"
